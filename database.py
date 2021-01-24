cc41058c560ebefeb123cfe9f156ea03c31c387b
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2021-01-24 20:03:21 +0200

    Modified database.py so that it works on Windows OS too.

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import platform
 
-
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+if platform.system() == 'Linux': 
+    engine = create_engine('sqlite:////test.db', convert_unicode=True)
+elif platform.system() == 'Windows':
+    engine = create_engine('sqlite:///test.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(
     autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
